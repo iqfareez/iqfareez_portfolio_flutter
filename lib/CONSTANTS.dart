ed168faa04731bce3964fec4b2b1859146902400
iqfareez <iqmal3@outlook.com>
2021-01-26 12:00:56 +0800

    added constants urls socmed etc

--- lib/CONSTANTS.dart
+++ lib/CONSTANTS.dart
@@ -1,5 +1,13 @@
 //urls
 const kFlutter = 'https://flutter.dev/';
+
+//Services
 const kGitHubProfile = 'https://github.com/iqfareez';
 const kPlayStoreDevPage =
     'https://play.google.com/store/apps/dev?id=9200064795631584674';
+
+//Social media
+const kInstagramUrl = 'https://www.instagram.com/iqfareez/';
+const kLinkedInUrl = 'https://www.linkedin.com/in/muhammad-iqfareez/';
+const kTwitterUrl = 'https://twitter.com/iqfareez2';
+const kEmailAddress = 'foxtrotiqmal3@gmail.com';
